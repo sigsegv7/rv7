9ee5d132e70d591958cbfbfb42ba8d34e561853d
Ian Moffett <ian@osmora.org>
2025-11-15 15:48:07 -0500

    sys: types: Add big long long types
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/inc/sys/types.h
+++ sys/inc/sys/types.h
@@ -39,10 +39,17 @@
 typedef unsigned char       uint8_t;
 typedef unsigned short      uint16_t;
 typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
 
 /* Signed types */
 typedef char       int8_t;
 typedef short      int16_t;
 typedef int        int32_t;
+typedef long long  int64_t;
+
+/* Size types */
+typedef uint64_t    size_t;
+typedef int64_t     ssize_t;
+typedef size_t      off_t;
 
 #endif  /* !_SYS_TYPES_H_ */
