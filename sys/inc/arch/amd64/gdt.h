fd43b12a711856e23e010b4b561ecc6aa6940074
Ian Moffett <ian@osmora.org>
2025-11-19 18:48:24 -0500

    kern/amd64: Move GDT selector defines to gdt.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sys/inc/arch/amd64/gdt.h
+++ sys/inc/arch/amd64/gdt.h
@@ -27,73 +27,12 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/errno.h>
-#include <mu/process.h>
-#include <mu/mmu.h>
-#include <vm/phys.h>
-#include <lib/string.h>
+#ifndef _MACHINE_GDT_H_
+#define _MACHINE_GDT_H_
 
-#define STACK_TOP 0xBFFFFFFF
+#define GDT_KERNCODE 0x08
+#define GDT_KERNDATA 0x10
+#define GDT_USERCODE 0x18
+#define GDT_USERDATA 0x20
 
-
-int
-mu_process_init(struct process *process, uintptr_t ip, int flags)
-{
-    struct trapframe *tf;
-    struct pcb *pcb;
-    uintptr_t stack_base;
-    uint8_t cs, ds;
-    int error;
-
-    if (process == NULL) {
-        return -EINVAL;
-    }
-
-    pcb = &process->pcb;
-    tf = &pcb->tf;
-
-    error = mu_pmap_forkvas(&pcb->vas);
-    if (error < 0) {
-        return error;
-    }
-
-    /* Allocate a new stack */
-    stack_base = vm_phys_alloc(1);
-    if (stack_base == 0) {
-        vm_phys_free(pcb->vas.cr3, 1);
-        return -ENOMEM;
-    }
-
-    /* Get the segment selectors based on mode */
-    if (ISSET(flags, PROC_KERN)) {
-        cs = GDT_KERNCODE;
-        ds = GDT_KERNDATA;
-    } else {
-        cs = GDT_USERCODE | 3;
-        ds = GDT_USERDATA | 3;
-    }
-
-    memset(tf, 0, sizeof(*tf));
-    tf->rip = ip;
-    tf->rflags = 0x202;
-    tf->cs = cs;
-    tf->ss = ds;
-
-    /* Map out the stack */
-    error = mu_pmap_map(
-        &pcb->vas,
-        stack_base,
-        STACK_TOP,
-        PROT_READ | PROT_WRITE,
-        PAGESIZE_4K
-    );
-
-    if (error < 0) {
-        vm_phys_free(stack_base, 1);
-        vm_phys_free(pcb->vas.cr3, 1);
-        return error;
-    }
-
-    tf->rsp = ALIGN_DOWN(STACK_TOP + (PAGESIZE - 1), 16);
-    return 0;
-}
+#endif  /* !_MACHINE_GDT_H_ */
